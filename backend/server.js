84c8b65aa8dd9fe768cc8aec33ae93008da3b9e6
thutanyan <thutathuta319@gmail.com>
2024-05-25 16:47:24 +0630

    Mongo Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // Require Module
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
 // ENV File Access
 require("dotenv").config();
@@ -14,10 +15,19 @@ app.use(morgan("dev"));
 
 
 // Recipes 
-app.use("/api/recipes",recipesRoutes);
+app.use("/api/recipes", recipesRoutes);
 
 
-// Server Running
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running at port:${process.env.PORT}`);
-})
+// Database connecting
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        console.log("Mongo database is connected");
+        // Server Running
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running at port:${process.env.PORT}`);
+        })
+    })
+    .catch((e) => {
+        console.log(`Mongo Connect-Part:${e.message}`);
+    })
+
